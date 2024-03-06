26e774aa5197a7f43f3a0dd0b629e27f1e0cba97
mertc <mertc@DESKTOP-8ID65LJ>
2024-03-06 15:17:16 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+using System.ComponentModel.Design;
+
+Random rastgeleSayiUretici = new Random();  // random sinifindan bir nesne olusturduk
+int rastgeleSayi = rastgeleSayiUretici.Next(1, 11); //1 dahil 11 haric, bu aralikta rastgele sayi uretir
+
+
+Console.WriteLine(" 1 ile 10 arasinda bir sayi tahmin edin. ");
+
+while (true)   // sonsuz dongu, dogru tahmin yapilana kadar dongu devam eder
+{
+    Console.WriteLine("Tahmininizi giriniz : ");
+    int tahmin = Convert.ToInt32(Console.ReadLine());
+
+    if (tahmin == rastgeleSayi)
+    {
+        Console.WriteLine("Tebrikler! Sayiyi dogru tahmin ettin.");
+        break;
+    }
+
+    else
+    {
+        Console.WriteLine("Yanlis tahmin, tekrar dene");
+    }
+
+}
+    
+ 
